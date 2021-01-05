a2d763dfaac79bc00cab8a821dedd16671fe8825
dmx78 <47801438+dmx78@users.noreply.github.com>
2021-01-05 20:03:12 +0700

    Changes On Login Form
    Add Image to Login Form

--- src/uLogin.pas
+++ src/uLogin.pas
@@ -15,7 +15,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg;
 
 type
   TfrmLogin = class(TForm)
